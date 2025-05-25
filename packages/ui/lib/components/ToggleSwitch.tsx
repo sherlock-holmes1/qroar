31e7d353665d39ade4ebaa50d108d3510b04706d
Vasilii L <bazil.lucky@gmail.com>
2025-05-25 18:21:22 -0400

    the design of the toggle switch is changed

--- packages/ui/lib/components/ToggleSwitch.tsx
+++ packages/ui/lib/components/ToggleSwitch.tsx
@@ -9,7 +9,9 @@ interface ToggleSwitchProps {
 export const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ checked, onChange, ariaLabel }) => (
   <label className="flex items-center cursor-pointer ml-4 text-right">
     <div
-      className={`relative w-11 h-6 mr-3 transition-colors duration-200 rounded-full ${checked ? 'bg-blue-500' : 'bg-gray-200'}`}>
+      className={`relative w-11 h-6 mr-3 transition-colors duration-200 rounded-full ${
+        checked ? 'bg-green-500' : 'bg-red-500'
+      }`}>
       <input
         type="checkbox"
         checked={checked}
@@ -18,8 +20,11 @@ export const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ checked, onChange, a
         aria-label={ariaLabel}
       />
       <div
-        className={`absolute top-0.5 transition-all duration-200 w-5 h-5 bg-white rounded-full shadow ${checked ? 'left-5' : 'left-0.5'}`}
-      />
+        className={`absolute top-0.5 transition-all duration-200 w-5 h-5 rounded-full shadow flex items-center justify-center text-white text-lg ${
+          checked ? 'left-5 bg-green-700' : 'left-0.5 bg-gray-700'
+        }`}>
+        {checked ? '✓' : '✗'}
+      </div>
     </div>
     <span className="sr-only">{ariaLabel}</span>
   </label>
