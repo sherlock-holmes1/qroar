36a04be7746c240083468707df04acbd5b19b66a
Vasilii L <bazil.lucky@gmail.com>
2025-06-10 21:04:53 -0400

    options screen refactoring. single state properties changed to an object

---
+++ packages/ui/lib/components/QRCodeBoxProps.tsx
@@ -0,0 +1,11 @@
+export interface QRCodeBoxProps {
+  qrText: string;
+  pathToLogo: string | undefined;
+  extension?: 'svg' | 'png' | 'jpeg' | 'webp';
+  style?: React.CSSProperties;
+  backgroundColor?: string;
+  foregroundColor?: string;
+  showGradient?: boolean;
+  gradientColor?: string;
+  cornersSquareType?: 'dot' | 'square' | 'extra-rounded';
+}
